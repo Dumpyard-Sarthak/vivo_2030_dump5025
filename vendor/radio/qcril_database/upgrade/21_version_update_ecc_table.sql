cb766c9a74e77b72f7c769785bd7c6353efbc5f9
sarthakroy2002 <sarthakroy2002@gmail.com>
2022-09-07 13:22:12 +0530

    Add vendor for qssi-user 12 SP1A.210812.003 compiler05152322 release-keys
    Signed-off-by: sarthakroy2002 <sarthakroy2002@gmail.com>

---
+++ vendor/radio/qcril_database/upgrade/21_version_update_ecc_table.sql
@@ -0,0 +1,14 @@
+/*
+  Copyright (c) 2019 Qualcomm Technologies, Inc.
+  All Rights Reserved.
+  Confidential and Proprietary - Qualcomm Technologies, Inc.
+*/
+
+BEGIN TRANSACTION;
+
+CREATE TABLE IF NOT EXISTS qcril_properties_table (property TEXT,value TEXT, PRIMARY KEY(property));
+INSERT OR REPLACE INTO qcril_properties_table (property, value) VALUES ('qcrildb_version', 21);
+
+INSERT OR REPLACE INTO qcril_properties_table VALUES('all_bc_msg','gsm');
+
+COMMIT TRANSACTION;
